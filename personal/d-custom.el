3122005ff82c80b7ed188d3790f4420007b01bc4
kendocode <git@kendocoding.com>
2018-08-07 08:49:36 -0500

    Tidy up config a bit

---
+++ personal/d-custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zop-to-char zenburn-theme yasnippet-snippets yari yaml-mode xref-js2 which-key web-mode volatile-highlights undo-tree solarized-theme smex smartrep smartparens smart-mode-line scss-mode ruby-tools robe rainbow-mode rainbow-delimiters ov operate-on-number neotree multi-term move-text markdown-mode+ magit key-chord json-mode js2-refactor imenu-anywhere ido-completing-read+ helm-swoop helm-projectile helm-descbinds helm-ag guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist geiser flycheck flx-ido expand-region elpa-mirror elisp-slime-nav editorconfig edit-server-htmlize easy-kill discover-my-major diminish diff-hl csv-mode crux company coffee-mode browse-kill-ring beacon anzu ag ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
